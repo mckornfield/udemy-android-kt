5067ef4f9b5cef59792ea400cd7cb9228e72f910
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-18 14:31:29 -0400

    vars

--- KotlinFun/app/src/main/java/com/mckornfield/kotlinfun/LearningKotlin.kt
+++ KotlinFun/app/src/main/java/com/mckornfield/kotlinfun/LearningKotlin.kt
@@ -1,5 +1,19 @@
 package com.mckornfield.kotlinfun
 
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
     println("Hello Boys!")
+
+    val age = 12
+
+    var candy = "Twix"
+
+    println(age)
+    println(candy)
+
+    candy = "Snickers"
+
+    println(candy)
+
+    val weather = "Party cloudy"
+    println(weather)
 }
