dd785a845e818a390511dc5175f2e5432981301b
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-18 15:13:37 -0400

    some maths

--- KotlinFun/app/src/main/java/com/mckornfield/kotlinfun/LearningKotlin.kt
+++ KotlinFun/app/src/main/java/com/mckornfield/kotlinfun/LearningKotlin.kt
@@ -1,15 +1,20 @@
 package com.mckornfield.kotlinfun
 
+/**
+ * Also they can be multiline
+ * like this comment here
+ */
 fun main(args: Array<String>) {
     println("Hello Boys!")
 
-    val age: Int = 12
+    // They can come at any time
+    val age: Int = 12 // Comments man
 
     var candy: String = "Twix"
 
     var thing : String
 
-    thing  = 123
+    thing  = "abc"
     println(age)
     println(candy)
 
@@ -19,4 +24,28 @@ fun main(args: Array<String>) {
 
     val weather = "Party cloudy"
     println(weather)
+
+    var wallet = 180
+    println(wallet)
+    wallet -= 60
+
+    println(wallet)
+
+    wallet += 100
+
+    println(wallet)
+
+    var pi: Double = 3.14
+
+    var radius = 5
+
+    var c = pi * radius.toDouble() * 2
+
+    println("%.2f".format(c))
+
+    var weight = 160
+
+    weight -= 5
+
+    println(weight)
 }
