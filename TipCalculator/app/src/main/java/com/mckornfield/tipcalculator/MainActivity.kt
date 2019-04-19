4eee2de9a17d1a71f74557fa9ad7b1a33c20f10f
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-18 22:05:42 -0400

    calculator done

--- TipCalculator/app/src/main/java/com/mckornfield/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/mckornfield/tipcalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mckornfield.tipcalculator
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        infoTextView.visibility = View.INVISIBLE
+
+        calculateButton.setOnClickListener {
+            val bill = billEditText.text.toString().toDouble()
+            val percentage = percentageEditText.text.toString().toDouble()
+
+            val tip = bill * (percentage/100)
+            val total = tip + bill
+
+            infoTextView.text = "Tip: ${toDollar(tip)}, Total: ${toDollar(total)}"
+            infoTextView.visibility = View.VISIBLE
+        }
+    }
+
+    fun toDollar(doubleVal: Double) : String {
+        return String.format("$%.2f",doubleVal)
     }
 }
